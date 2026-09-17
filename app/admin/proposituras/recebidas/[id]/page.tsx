0f8893c94db8c748fc8be4589e8163a1bc12f348
felipegabrielsouza <fegabriel2026@gmail.com>
2026-09-17 13:53:24 -0300

    fix: corrige caminho de importacao do prisma nas subpastas

--- app/admin/proposituras/recebidas/[id]/page.tsx
+++ app/admin/proposituras/recebidas/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma'
+import { prisma } from '../../../../lib/prisma'
 import { redirect } from 'next/navigation'
 import { writeFile } from 'fs/promises'
 import path from 'path'
