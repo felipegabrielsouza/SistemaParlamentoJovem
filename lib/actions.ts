9cb28fc066165105e96c1bac0d45bd7cd2bf7826
felipegabrielsouza <fegabriel2026@gmail.com>
2026-09-18 11:42:55 -0300

    fix: adiciona className padrao na criacao do parlamentar para evitar null constraint

--- lib/actions.ts
+++ lib/actions.ts
@@ -65,6 +65,7 @@ export async function createParliamentarianAction(formData: FormData) {
           create: {
             name: name,
             school: school,
+            className: "Turma Única",
           } as any
         }
       }
